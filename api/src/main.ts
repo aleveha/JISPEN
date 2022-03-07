beb6b1928d7d2f111b9c44a9b13ab37eeb367ae3
aleveha <21022001qQ>
2022-03-07 20:29:18 +0100

    feat: edit api port to 8000 #0

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function main() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix("api");
-	await app.listen(3000);
+	await app.listen(8000);
 }
 
 main();
