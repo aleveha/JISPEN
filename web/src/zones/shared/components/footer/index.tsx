a95de85e97c7794a1b37d20a28271ab9a8bc5e6a
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-06-03 16:04:04 +0200

    feat: add user documentation file #0

--- web/src/zones/shared/components/footer/index.tsx
+++ web/src/zones/shared/components/footer/index.tsx
@@ -3,7 +3,13 @@ import React, { FC } from "react";
 export const Footer: FC = () => {
 	return (
 		<div className="w-full h-12 bg-primary flex justify-between px-20">
-			<h4 className="leading-[2.6] text-lg text-white text-center">Nápověda</h4>
+			<a
+				className="leading-[2.6] text-lg text-white text-center"
+				href="/static/Uživatelská dokumentace.pdf"
+				target="_blank"
+			>
+				Nápověda
+			</a>
 			<p className="leading-[2.6] text-lg text-white text-center">
 				TAČR,&nbsp;Technická univerzita v&nbsp;Liberci, INISOFT Consulting s.r.o.
 			</p>
