594e1000a785e6419db45af9474b86d97deeea96
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-10-23 21:24:49 +0200

    feat(web): add progress bar

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,10 +2,30 @@ import { ThemeProvider } from "@mui/material";
 import { THEME } from "@styles/theme";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import Nprogress from "nprogress";
+import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	const { events } = useRouter();
+
+	useEffect(() => {
+		const onRouteChangeStart = () => Nprogress.start();
+		const onRouteChangeStop = () => Nprogress.done();
+
+		events.on("routeChangeStart", onRouteChangeStart);
+		events.on("routeChangeComplete", onRouteChangeStop);
+		events.on("routeChangeError", onRouteChangeStop);
+
+		return () => {
+			events.off("routeChangeStart", onRouteChangeStart);
+			events.off("routeChangeComplete", onRouteChangeStop);
+			events.off("routeChangeError", onRouteChangeStop);
+		};
+	}, [events]);
+
 	return (
 		<>
 			<Head>
