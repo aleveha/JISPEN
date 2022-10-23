f62035369d67fa174db05d7486100c843d4c50f0
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-10-23 18:33:08 +0200

    feat(web): add server side data fetching to new record page

--- web/pages/records/new.tsx
+++ web/pages/records/new.tsx
@@ -1,3 +1,24 @@
-import CreateRecordPage from "@pages/records/new-record-page";
+import { apiServer } from "@api/config";
+import { fetcher } from "@api/index";
+import { Template } from "@api/templates/types";
+import { Page } from "@pages/records/new-record-page";
+import { GetServerSideProps } from "next";
 
-export default CreateRecordPage;
+export const getServerSideProps: GetServerSideProps = async ctx => {
+	const accessToken = ctx.req.cookies["accessToken"];
+	const { data, error } = await fetcher<Template[]>({
+		axiosInstance: apiServer,
+		method: "get",
+		url: "/template/all",
+		accessToken,
+	});
+
+	return {
+		props: {
+			data: data ?? null,
+			error: error ?? null,
+		},
+	};
+};
+
+export default Page;
