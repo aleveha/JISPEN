2e6049321dd8f33ad853a69a052cd76875c5a166
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-10-25 11:23:02 +0200

    feat(web): add useSWR for data revalidation

--- web/src/pages/templates/all-templates-page.tsx
+++ web/src/pages/templates/all-templates-page.tsx
@@ -1,18 +1,46 @@
+import { apiClient } from "@api/config";
 import { Template } from "@api/templates/types";
 import { Button } from "@shared/components/button/button";
 import { withDashboardLayout } from "@shared/components/layout/layout";
 import { DiscriminatedUnion } from "@shared/types/types";
 import { TemplatesTable } from "@zones/templates/components/templates-table";
+import { getCookie } from "cookies-next";
 import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { memo, useCallback, useEffect } from "react";
 import { toast } from "react-hot-toast";
+import useSWR, { Fetcher, SWRConfig } from "swr";
 
-const AllTemplatesPageComponent: NextPage<DiscriminatedUnion<Template[]>> = ({ data, error }) => {
+const AllTemplates = memo(() => {
+	const accessToken = getCookie("accessToken") as string;
+	const allTemplatesFetcher = useCallback<Fetcher<Template[], string>>(
+		(url: string) => apiClient.get<Template[]>(url, { headers: { Authorization: `Bearer ${accessToken}` } }).then(res => res.data),
+		[accessToken]
+	);
+
+	const { data: templates } = useSWR<Template[]>("/templates/all", allTemplatesFetcher);
+
+	return (
+		<div className="h-max">
+			{!templates || templates.length == 0 ? (
+				<p className="text-xl text-grey">Zatím nemáte žádné šablony</p>
+			) : (
+				<TemplatesTable templates={templates} />
+			)}
+			<div className="mt-8 flex w-full items-center justify-end">
+				<Link href="/templates/new" passHref>
+					<Button variant="primary">Nová šablona</Button>
+				</Link>
+			</div>
+		</div>
+	);
+});
+
+AllTemplates.displayName = "AllTemplates";
+
+const AllTemplatesPageComponent: NextPage<DiscriminatedUnion<Template[]>> = ({ data: templates, error }) => {
 	const router = useRouter();
-	const [templates, setTemplates] = useState<Template[] | null>(data ?? null);
-	const onDataChanged = useCallback((templates: Template[]) => setTemplates(templates), []);
 
 	useEffect(() => {
 		if (error) {
@@ -30,18 +58,9 @@ const AllTemplatesPageComponent: NextPage<DiscriminatedUnion<Template[]>> = ({ d
 	}
 
 	return (
-		<div className="h-max">
-			{templates.length == 0 ? (
-				<p className="text-xl text-grey">Zatím nemáte žádné šablony</p>
-			) : (
-				<TemplatesTable data={templates} onDataChange={onDataChanged} />
-			)}
-			<div className="mt-8 flex w-full items-center justify-end">
-				<Link href="/templates/new" passHref>
-					<Button variant="primary">Nová šablona</Button>
-				</Link>
-			</div>
-		</div>
+		<SWRConfig value={{ fallback: { "/templates/all": templates } }}>
+			<AllTemplates />
+		</SWRConfig>
 	);
 };
 
