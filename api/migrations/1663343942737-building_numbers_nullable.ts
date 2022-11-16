f35e12445cfec169c3f94e07f7f48e3c50dd6c2f
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-11-16 11:59:10 +0100

    fix(db): update migrations (waste init)

--- api/migrations/1663343942737-building_numbers_nullable.ts
+++ api/migrations/1663343942737-building_numbers_nullable.ts
@@ -4,11 +4,6 @@ export class buildingNumbersNullable1663343942737 implements MigrationInterface 
 	name = "buildingNumbersNullable1663343942737";
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(`
-            INSERT INTO waste("uid", "name", "category")
-            VALUES ('150101', 'Papírové a lepenkové obaly', 'O')
-		`);
-
 		await queryRunner.query(`ALTER TABLE "address"
             ALTER COLUMN "registry_number" DROP NOT NULL`);
 		await queryRunner.query(`ALTER TABLE "address"
