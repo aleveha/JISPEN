dde6ea58d12ff7792e843efb32bcb338150a8dbc
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-11-28 12:47:45 +0100

    feat(web): add PWA

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,13 @@
-// eslint-disable-next-line no-undef
-module.exports = {
+// eslint-disable-next-line no-undef,@typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+	disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
 	experimental: {
 		outputStandalone: true,
 	},
-};
+});
--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -16,14 +16,21 @@ export default class MyDocument extends Document {
 					/>
 					<meta name="title" content="Průběžná evidence odpadů | JISPEN" />
 					<meta name="og:image" content={`${process.env.NEXT_PUBLIC_API_URL}/static/images/og-image.png`} />
+					<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
 					<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;700&display=swap" rel="stylesheet" />
-					<link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
-					<link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-					<link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-					<link rel="manifest" href="/static/site.webmanifest" />
-					<link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#3b82f6" />
+					<link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+					<link rel="manifest" href="/static/icons/site.webmanifest" crossOrigin="use-credentials" />
+					<link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#140939" />
+					<link rel="shortcut icon" href="/static/icons/favicon.ico" />
+					<meta name="apple-mobile-web-app-title" content="JISPEN" />
+					<meta name="application-name" content="JISPEN" />
+					<meta name="msapplication-TileColor" content="#fff" />
+					<meta name="msapplication-config" content="/static/icons/browserconfig.xml" />
+					<meta name="theme-color" content="#fff" />
 				</Head>
 				<body>
 					<Main />
