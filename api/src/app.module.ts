e40532fdc2ced29de8bfa16a1a42baa9891fcf33
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-12-07 15:04:35 +0100

    fix(api): add captcha alias to package.json

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { AuthModule } from "@auth/auth.module";
+import { CaptchaModule } from "@captcha/captcha.module";
 import { mailerConfig } from "@configs/mailer.config";
 import { postgresConfig } from "@configs/postgres.config";
 import { ExportModule } from "@export/export.module";
@@ -10,7 +11,6 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { RecordsModule } from "@records/records.module";
 import { TemplateModule } from "@template/template.module";
 import { UserModule } from "@user/user.module";
-import { CaptchaModule } from "./captcha/captcha.module";
 
 const STAGE = process.env.NODE_ENV;
 
