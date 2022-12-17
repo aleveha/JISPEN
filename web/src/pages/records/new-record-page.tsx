d028803e70549072b76abd68397b4beb6b36db11
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-12-17 17:26:16 +0100

    feat(web): add useSWR to new record page

--- web/src/pages/records/new-record-page.tsx
+++ web/src/pages/records/new-record-page.tsx
@@ -1,13 +1,32 @@
+import { apiClient } from "@api/config";
 import { Template } from "@api/templates/types";
 import { withDashboardLayout } from "@shared/components/layout/layout";
 import { DiscriminatedUnion } from "@shared/types/types";
 import { EditRecordForm } from "@zones/records/forms/edit-record-form";
+import { getCookie } from "cookies-next";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { toast } from "react-hot-toast";
+import useSWR, { Fetcher, SWRConfig } from "swr";
 
-const CreateRecordPage: NextPage<DiscriminatedUnion<Template[]>> = ({ data: templates, error }) => {
+const CreateRecord = () => {
+	const accessToken = getCookie("access_token") as string;
+	const allTemplatesFetcher = useCallback<Fetcher<Template[], string>>(
+		(url: string) => apiClient.get<Template[]>(url, { headers: { Authorization: `Bearer ${accessToken}` } }).then(res => res.data),
+		[accessToken]
+	);
+
+	const { data: templates } = useSWR<Template[]>("/record/new", allTemplatesFetcher);
+
+	if (!templates) {
+		return null;
+	}
+
+	return <EditRecordForm templates={templates} />;
+};
+
+const CreateRecordComponent: NextPage<DiscriminatedUnion<Template[]>> = ({ data: templates, error }) => {
 	const router = useRouter();
 
 	useEffect(() => {
@@ -25,7 +44,11 @@ const CreateRecordPage: NextPage<DiscriminatedUnion<Template[]>> = ({ data: temp
 		return null;
 	}
 
-	return <EditRecordForm templates={templates} />;
+	return (
+		<SWRConfig value={{ fallback: { "/record/new": templates } }}>
+			<CreateRecord />
+		</SWRConfig>
+	);
 };
 
-export const Page = withDashboardLayout(CreateRecordPage, "Nový záznam");
+export const Page = withDashboardLayout(CreateRecordComponent, "Nový záznam");
